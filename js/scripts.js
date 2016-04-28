6079d94a6dd12866cd15fc27d18ab2465ad058c6
veronicamarsh-volusion <veronica_marsh@volusion.com>
2016-04-28 15:23:18 -0500

    Finished tutorial coding and added additional JS line to keep tasks showing when page refreshed

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,65 @@
-var addTaskButton = document.getElementById("add-task-button");
-var newTaskInput = document.getElementById("new-task-input");
-var todolistContainer = document.getElementById("todolist-container");
-var templateElement = document.getElementById("list-item-template");
-var template = templateElement.innerHTML;
+window.Todo = {
+  addTaskButton: document.getElementById("add-task-button"),
+  newTaskInput: document.getElementById("new-task-input"),
+  container: document.getElementById("todolist-container"),
+  template: document.getElementById("list-item-template").innerText,
+  setup: function () {
+    Todo.addTaskButton.addEventListener('click', Todo.onAddTaskClicked); 
+    Todo.container.addEventListener('click', Todo.onTodolistClicked);
 
-  addTaskButton.addEventListener('click', function(event) {
-      var taskName = newTaskInput.value;
-      newTaskInput.value = "";
+    Todo.renderTasks();
+  },
+  onAddTaskClicked: function(event) {
+    var taskName = Todo.newTaskInput.value;
+    Todo.newTaskInput.value = "";
 
-      var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
-      todolistContainer.insertAdjacentHTML('afterbegin', taskHTML);
-});
+    var taskHTML = Todo.template.replace("<!-- TASK_NAME -->", taskName);
+    Todo.container.insertAdjacentHTML('afterbegin', taskHTML);
 
+    Todo.saveTask(taskName, false);
+  },
+  onTodolistClicked: function(event) {
+    var targetElement = event.toElement;
 
-todolistContainer.addEventListener('click', function(event) {
-  var targetElement = event.toElement;
+    while (!targetElement.classList.contains("task")) {
+        targetElement = targetElement.parentElement;
+    }
+  
+    var checkbox = targetElement.querySelector(".checkbox");
 
-  while (!targetElement.classList.contains("task")) {
-    targetElement = targetElement.parentElement;
+    if (checkbox.checked) {
+      targetElement.classList.add("completed");
+    } else {
+        targetElement.classList.remove("completed");
+      }
+
+    var taskNameElement = targetElement.querySelector(".task-name");
+    var taskName = taskNameElement.innerText;
+
+    Todo.saveTask(taskName, checkbox.checked);
+  },
+  saveTask: function(name, isCompleted) {
+    window.localStorage.setItem(name, isCompleted);
+  },
+  renderTasks: function() {
+    for (var i = 0; i < window.localStorage.length; i++) {
+      var taskName = window.localStorage.key(i);
+      var isCompleted = window.localStorage.getItem(taskName) == "true";
+      var taskHTML = Todo.template.replace("<!-- TASK_NAME -->", taskName);
+
+      if (!isCompleted) {
+        Todo.container.insertAdjacentHTML('afterbegin', taskHTML);
+      } else {
+
+          Todo.container.insertAdjacentHTML('afterbegin',taskHTML);
+          
+      }
+    }
   }
+}
 
-  var checkbox = targetElement.querySelector(".checkbox");
+Todo.setup();
 
-  if (checkbox.checked) {
-    targetElement.classList.add("completed");
-  } else {
-    targetElement.classList.remove("completed");
-  }
-});
 
 
 
